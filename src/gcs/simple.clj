6dec30bdc3378b814a0238a184eeec46221f1be8
Ivan Kuznetsov <me@jeiwan.ru>
2020-01-20 09:54:54 +0700

    Update 'gcs.simple' to upload a public file to GCS

--- src/gcs/simple.clj
+++ src/gcs/simple.clj
@@ -1,12 +1,22 @@
-(ns gcs.simple)
+(ns gcs.simple
+  (:import [com.google.cloud.storage BlobId BlobInfo Storage$BlobTargetOption StorageOptions]
+           [com.google.cloud.storage Acl Acl$Role Acl$User]))
 
-(import com.google.auth.oauth2.ServiceAccountCredentials
-      [com.google.cloud.storage BlobId BlobInfo BucketInfo Storage Storage$BlobTargetOption StorageOptions])
+(def ^:static bucket-name "spark-static-pow")
+(def ^:static file-name "simple.html")
 
-(def blob-id (BlobId/of bucket-name "x"))
+(def blob-id (BlobId/of bucket-name file-name))
 
 (def storage (.getService (StorageOptions/getDefaultInstance)))
 
-(def blob-info (-> (BlobInfo/newBuilder blob-id) (.setContentType "text/plain") .build))
+(def blob-info
+  (-> (BlobInfo/newBuilder blob-id)
+      (.setContentType "text/html")
+      (.setAcl (list (Acl/of (Acl$User/ofAllUsers) Acl$Role/READER)))
+      .build))
 
-(.create storage blob-info (.getBytes "hello cloud storage") (into-array Storage$BlobTargetOption []))
+(defn -main [& _]
+  (.create storage
+           blob-info
+           (.getBytes "<!DOCTYPE html><html><body><h1>SIMPLE</h1></body></html>")
+           (into-array Storage$BlobTargetOption [])))
