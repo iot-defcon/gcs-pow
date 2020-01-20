f51c0b82325d964f5dcaa4b1ee0e6a1ea6ef870a
Ivan Kuznetsov <me@jeiwan.ru>
2020-01-20 10:46:14 +0700

    Set a delete lifecycle rule

--- src/gcs/simple.clj
+++ src/gcs/simple.clj
@@ -1,5 +1,5 @@
 (ns gcs.simple
-  (:import [com.google.cloud.storage BlobId BlobInfo Storage$BlobTargetOption StorageOptions]
+  (:import [com.google.cloud.storage BlobId BlobInfo BucketInfo$LifecycleRule BucketInfo$LifecycleRule$LifecycleAction BucketInfo$LifecycleRule$LifecycleCondition Storage$BlobTargetOption Storage$BucketGetOption Storage$BucketTargetOption StorageOptions]
            [com.google.cloud.storage Acl Acl$Role Acl$User]))
 
 (def ^:static bucket-name "spark-static-pow")
@@ -16,7 +16,22 @@
       (.setCacheControl "public, max-age=10800")
       .build))
 
+(defn set-lifecycle []
+  (let [delete-rule (BucketInfo$LifecycleRule. (BucketInfo$LifecycleRule$LifecycleAction/newDeleteAction)
+                                               (-> (BucketInfo$LifecycleRule$LifecycleCondition/newBuilder)
+                                                   (.setAge (int 1))
+                                                   .build))
+        bucket (.get storage bucket-name (into-array Storage$BucketGetOption []))]
+    (if (empty? (.getLifecycleRules bucket))
+      (-> bucket
+        .toBuilder
+        (.setLifecycleRules (list delete-rule))
+        .build
+        (.update (into-array Storage$BucketTargetOption [])))
+      (println "bucket has some lifecycle rules"))))
+
 (defn -main [& _]
+  (set-lifecycle)
   (.create storage
            blob-info
            (.getBytes "<!DOCTYPE html><html><body><h1>SIMPLE</h1></body></html>")
